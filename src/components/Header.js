29cf4d21a484fa737d167024a3ec962dcc3f6114
tranttuhao <hao.tran@fromlabs.com>
2021-11-29 21:55:07 +0700

    add loader

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,12 @@ const pathVariants = {
 const Header = () => {
   return (
     <header>
-      <div className="logo">
+      <motion.div
+        className="logo"
+        drag
+        dragConstraints={{ left: 0, top: 0, bottom: 0, right: 0 }}
+        dragElastic={1}
+      >
         <motion.svg
           className="pizza-svg"
           xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +58,7 @@ const Header = () => {
             variants={pathVariants}
           />
         </motion.svg>
-      </div>
+      </motion.div>
       <motion.div
         className="title"
         initial={{ y: -250 }}
---
+++ src/components/Loader.js
@@ -0,0 +1,49 @@
+import { motion, useCycle } from 'framer-motion';
+import React from 'react';
+
+const loaderVariants = {
+  animationOne: {
+    x: [-20, 20],
+    y: [0, -30],
+    transition: {
+      x: {
+        yoyo: Infinity,
+        duration: 0.5,
+      },
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: 'easeOut',
+      },
+    },
+  },
+  animationTwo: {
+    x: 0,
+    y: [0, -30],
+    transition: {
+      y: {
+        yoyo: Infinity,
+        duration: 0.25,
+        ease: 'easeOut',
+      },
+    },
+  },
+};
+
+const Loader = () => {
+  const [animation, cycleAnimation] = useCycle('animationOne', 'animationTwo');
+  return (
+    <>
+      <motion.div
+        className="loader"
+        variants={loaderVariants}
+        animate={animation}
+      ></motion.div>
+      <div onClick={() => cycleAnimation()} style={{ cursor: 'pointer' }}>
+        Cycle loader
+      </div>
+    </>
+  );
+};
+
+export default Loader;
